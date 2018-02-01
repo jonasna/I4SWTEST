736560683412da28520610532fbb29e7e55b4024
Junni17 <jonas-na@hotmail.dk>
2018-02-01 11:07:29 +0100

    Add project files.

---
+++ Lektion01.UnitTest.Old/CalculatorTest.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lektion01.UnitTest.Old
+{
+    class CalculatorTest
+    {
+        static void Main(string[] args)
+        {
+            var calculator = new Lektion01.Calculator();
+
+            Console.WriteLine("Add:");
+            Console.WriteLine("Add({0},{1})\t = {2,7}", 5, 3, calculator.Add(5,3));
+            Console.WriteLine("Add({0},{1})\t = {2,7}", 5, -3, calculator.Add(5, -3));
+            Console.WriteLine("Add({0},{1})\t = {2,7}", -5, -3, calculator.Add(-5, -3));
+            Console.WriteLine();
+
+            Console.WriteLine("Subtract:");
+            Console.WriteLine("Subtract({0},{1})\t = {2,7}", 5, 3, calculator.Subtract(5, 3));
+            Console.WriteLine("Subtract({0},{1})\t = {2,7}", 5, -3, calculator.Subtract(5, -3));
+            Console.WriteLine("Subtract({0},{1})\t = {2,7}", -5, -3, calculator.Subtract(-5, -3));
+            Console.WriteLine();
+
+            Console.WriteLine("Multiply:");
+            Console.WriteLine("Multiply({0},{1})\t = {2,7}", 5, 3, calculator.Multiply(5, 3));
+            Console.WriteLine("Multiply({0},{1})\t = {2,7}", 5, -3, calculator.Multiply(5, -3));
+            Console.WriteLine("Multiply({0},{1})\t = {2,7}", -5, -3, calculator.Multiply(-5, -3));
+            Console.WriteLine();
+
+            Console.WriteLine("Power:");
+            Console.WriteLine("Power({0},{1})\t = {2,7}", 5, 3, calculator.Power(5, 3));
+            Console.WriteLine("Power({0},{1})\t = {2,7}", 5, -3, calculator.Power(5, -3));
+            Console.WriteLine("Power({0},{1})\t = {2,7}", -5, -3, calculator.Power(-5, -3));
+            Console.WriteLine();
+        }
+    }
+}
---
+++ Lektion01.UnitTests/CalculatorTests.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace Lektion01.UnitTests
+{
+    [TestFixture]
+    public class CalculatorTests
+    {
+        [Test]
+        public void Add_2And4_Returns6()
+        {
+            var uut = new Calculator();
+
+            Assert.That(uut.Add(2,4), Is.EqualTo(6));
+        }
+
+        [Test]
+        public void Add_Minus5And2_ReturnsMinus3()
+        {
+            var uut = new Calculator();
+
+            Assert.That(uut.Add(-5,2), Is.EqualTo(-3));
+        }
+
+        [Test]
+        public void Add_Minus2AndMinus4_ReturnsMinus6()
+        {
+            var uut = new Calculator();
+
+            Assert.That(uut.Add(-2,-4), Is.EqualTo(-6));
+        }
+
+        [Test]
+        public void Subtract_2And4_ReturnsMinus2()
+        {
+            var uut = new Calculator();
+            
+            Assert.That(uut.Subtract(2,4), Is.EqualTo(-2));
+        }
+
+        [TestCase(3, -5, 8)]
+        [TestCase(-3,-3, 0)]
+        public void Subtract_Multiple_ReturnsCorrect(double a, double b, double c)
+        {
+            var uut = new Calculator();
+            Assert.That(uut.Subtract(a,b), Is.EqualTo(c));
+        }
+
+        [TestCase(3, 3, 9)]
+        [TestCase(-3,3,-9)]
+        [TestCase(-3,-3,9)]
+        public void Multiply_Multiple_ReturnsCorrect(double a, double b, double c)
+        {
+            var uut = new Calculator();
+            Assert.That(uut.Multiply(a,b), Is.EqualTo(c));
+        }
+
+        [TestCase(2,2,4)]
+        [TestCase(-2,2,4)]
+        [TestCase(1,-2, 1)]
+        public void Power_Multple_ReturnsCorrect(double a, double b, double c)
+        {
+            var uut = new Calculator();
+            Assert.That(uut.Power(a,b), Is.EqualTo(c));
+        }
+    }
+}
